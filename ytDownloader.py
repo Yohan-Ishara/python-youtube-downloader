fdaa254304f4306a9625a461d1064d0b380a2c27
Yohan Ishara <yohanishara01@gmail.com>
2022-12-29 14:53:21 +0530

    create canvas

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,4 +1,25 @@
 from pytube import YouTube
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
+
+screen = Tk()
+title = screen.title('YouTube Downloader')
+canvas = Canvas(screen, width=500, height=300)
+canvas.pack()
+img = Image.open('images/down.png')
+resized_img = img.resize((500, 300), Image.LANCZOS)
+new_image = ImageTk.PhotoImage(resized_img)
+# logo_img= logo_img.subsample(2,2)
+canvas.create_image(0, 0, anchor=NW, image=new_image)
+
+
+link_field = Entry(screen, width=30)
+link_label = Label(screen, text="Put your link here")
+
+canvas.create_window(100, 100, window=link_label)
+canvas.create_window(100, 140, window=link_field)
+screen.mainloop()
 
 
 def download_video(link):
@@ -7,7 +28,7 @@ def download_video(link):
     try:
         youtube_object.download()
     except:
-        print('Cannot fount video')
+        print('Cannot found video')
     print('Download Completed')
 
 
