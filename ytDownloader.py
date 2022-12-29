482c6864436376a96f5f39ef436bfff08dc41ce4
Yohan Ishara <yohanishara01@gmail.com>
2022-12-29 13:59:22 +0530

    base implmented

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,14 +1,15 @@
 from pytube import YouTube
 
 
-def download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+def download_video(link):
+    youtube_object = YouTube(link)
+    youtube_object = youtube_object.streams.get_highest_resolution()
     try:
-        youtubeObject.download()
+        youtube_object.download()
     except:
         print('Cannot fount video')
     print('Download Completed')
 
+
 link = input('Put your link here: ')
-download(link)
+download_video(link)
